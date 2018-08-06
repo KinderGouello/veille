62fa02ece673fea71bddc8ae42d09b03567727a8
Rémy Gouello <rigouello@gmail.com>
2018-08-06 22:10:11 +0200

    remove useles files and add condition to .env

--- server/digests/twitter.js
+++ server/digests/twitter.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV === 'production') require('dotenv').config();
 const twitter = require('../utils/twitter-api');
 const moment = require('moment');
 const R = require('ramda');
--- server/utils/twitter-api.js
+++ server/utils/twitter-api.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV === 'production') require('dotenv').config();
 const Twitter = require('twitter');
 const moment = require('moment');
 
